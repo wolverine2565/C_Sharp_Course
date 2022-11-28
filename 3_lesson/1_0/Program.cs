7ff402b9039262e81e96db143b45d8c14207ea83
email <wolverine89@bk.ru>
2022-11-28 21:14:50 +0300

    3_lesson

---
+++ 3_lesson/1_0/Program.cs
@@ -0,0 +1,15 @@
+﻿void Quarters(string a)
+{
+    if (a == "I")
+        Console.WriteLine("x > 0 and y > 0");
+    else if (a == "II")
+        Console.WriteLine("x < 0 and y > 0");
+    else if (a == "III")
+        Console.WriteLine("x < 0 and y < 0");   
+    else if (a == "IV")
+        Console.WriteLine("x > 0 and y <  0");
+    else 
+        Console.WriteLine("error");       
+
+}
+Quarters(Console.ReadLine());
