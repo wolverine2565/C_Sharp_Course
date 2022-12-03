461d6aada36ff8cc5118e75f5d654a118f2444ab
email <wolverine89@bk.ru>
2022-12-04 00:08:45 +0300

    homework

---
+++ 3_lesson/homework/Task_01/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да*/
+
+int a = int.Parse(Console.ReadLine());
+string s = a.ToString();
+char[] ar = s.ToCharArray();
+Array.Reverse(ar);
+s = new String(ar);
+int an = Convert.ToInt32(s);
+int digitCount = (int)Math.Log10(a) + 1;
+    if (digitCount != 5)
+        Console.WriteLine("Число не является 5-значным");
+        else    
+            if (a == an)
+            Console.WriteLine("да");  
+            else
+            Console.WriteLine("нет");
+
+
---
+++ 3_lesson/homework/Task_03/Program.cs
@@ -0,0 +1,29 @@
+﻿/*Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125*/
+
+void count(int N)
+{
+   int i = 0;
+   double result;
+   while (i < N )
+   {
+        i += 1;
+        if (i == (N))
+        {
+            result = Math.Pow(i,3);
+            Console.Write($"{result}");
+        }
+        else
+        {
+        
+        result = Math.Pow(i,3);
+        Console.Write($"{result},");
+        }
+   }
+    
+   
+}
+Console.WriteLine("Введите число");
+int n = int.Parse(Console.ReadLine());
+count(n);
