fa52b30653dc10ef058ae9ffd19c722cc1fb1962
email <wolverine89@bk.ru>
2022-12-15 20:17:37 +0300

    Homework_6

---
+++ 6_lesson/homework/Task_01/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Задача 1: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3*/
+
+int CountNum()
+{
+    int count = 0;
+    string word;
+    
+    while (true)
+    {
+        Console.Write("Введите число N ");
+        word = Console.ReadLine();
+
+        if (word == "") return count;
+        else if (int.Parse(word) > 0) count = count + 1;
+    }
+    
+}
+
+Console.Write(CountNum());
---
+++ 6_lesson/homework/Task_02/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Задача 2: Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)*/
+
+void Method1(double k1, double b1, double k2, double b2)
+{
+    double k_sub = k1 - k2; 
+    double x = (b2 - b1)/ (k1 - k2);
+    double y = k1 * x + b1;
+
+    if (k_sub != 0)
+    {
+        Console.WriteLine($"{x}; {y}");
+    }
+    else if (k1 == k2 && b1 == b2)
+        Console.WriteLine(" Существует бесконечное множество точек пересечения");
+    else
+        Console.WriteLine(" Эти линии не пересекаются");
+}
+Method1(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()));
